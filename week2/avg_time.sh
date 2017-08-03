6b863d5055290eff09cca0df943cbeeb74f1d07a
Toby Huang <619058900@qq.com>
2017-08-03 18:25:57 +1000

    finished week2's work

--- week2/avg_time.sh
+++ week2/avg_time.sh
@@ -3,11 +3,12 @@
 cut -d'm' -f1 "$1" > foo.tmp
 #  calculate the sum of minutes
 min_sum=0
-count=0
+
+# get the experiment times
+count=$(wc foo.tmp |cut -d' ' -f2)
 while read line
 do
-    min_sum=$(echo "$sum+$line" | bc -l)
-    count++
+    min_sum=$(echo "$min_sum+$line" | bc -l)
 done < foo.tmp
 
 # calculate the sum of sec
@@ -15,10 +16,9 @@ cut -d'm' -f2 "$1" | tr -d 's'> foo.tmp
 sec_sum=0
 while read line
 do
-    sec_sum=$(echo "$sum+$line" | bc -l)
+    sec_sum=$(echo "$sec_sum+$line" | bc -l)
 done < foo.tmp
 
 
 # calculate the total sum
-echo min_sum
-echo sec_sum
+echo "$count $min_sum $sec_sum" | ./avg_sec
--- week2/time.sh
+++ week2/time.sh
@@ -1,60 +1,84 @@
 i=1
+test_count=233
 
+# print the promt
+printf "Input\tInitial\t Has\tNumber\tAvg Time\tAvg Time\n"
+printf "size\tOrder\t Dup.\tof runs\tfor usel\tfor sort\n"
 
-for (( mod = 0; mod < 1; mod++ )); do
-    #every mod test 100 times
+# control the input_size
+for input_size in 5000 10000 25000 50000 100000
+do
 
+    for (( mod = 0; mod < 6; mod++ )); do
+        #every mod test 100 times
 
-    # sort for each mod and record
-    while [ $i -le 100 ]
-    do
-        # generate the data for each time of sort
-        if [[ $mod==0 ]]; then
+        i=1
+        # sort for each mod and record
+        while [ $i -le $test_count ]
+        do
+            # generate the data for each time of sort
+            if [[ mod -eq 0 ]]; then
+                #random + no duplicates
+                seq $input_size | sort -R >source.tmp
+            elif [[ mod -eq 1 ]]; then
+                # sorted + no duplicates
+                seq $input_size | sort -n >source.tmp
+            elif [[ mod -eq 2 ]]; then
+                # reverse + no duplicates
+                seq $input_size | sort -nr >source.tmp
+            elif [[ mod -eq 3 ]]; then
+                # random + has duplicates
+                seq $input_size | sort -R >source.tmp
+            elif [[ mod -eq 4 ]]; then
+                # sorted + has duplicates
+                seq $input_size | sort -n >source.tmp
+            elif [[ mod -eq 5 ]]; then
+                # reverse + has duplicates
+                seq $input_size | sort -nr >source.tmp
+            fi
+
+            # runing the sort
+            (time sort -n) < source.tmp 2>sys.tmp > /dev/null
+            grep real sys.tmp | cut -f2 >> sys_all.tmp
+            (time ./usel) < source.tmp 2>user.tmp > /dev/null
+            grep real user.tmp | cut -f2 >> user_all.tmp
+            i=`expr $i + 1`
+        done
+
+        # calculate the avg time for each method and records
+        user=`./avg_time.sh user_all.tmp`
+        sys=`./avg_time.sh sys_all.tmp`
+
+        # #  clean the record files
+        # rm -rf sys_all.tmp user_all.tmp
+
+        # print the result in the file
+
+        # write the data into files
+        if [[ mod -eq 0 ]]; then
             #random + no duplicates
-            seq 1000 | sort -R >source.tmp
-        elif [[ $mod==1 ]]; then
+            printf "%d\trandom \t no\t" $input_size
+        elif [[ mod -eq 1 ]]; then
             # sorted + no duplicates
-            seq 1000 | sort -n >source.tmp
-        elif [[ $mod==2 ]]; then
+            printf "%d\tsorted \t no\t" $input_size
+        elif [[ mod -eq 2 ]]; then
             # reverse + no duplicates
-            seq 1000 | sort -nr >source.tmp
-        elif [[ $mod==3 ]]; then
+            printf "%d\treverse\t no\t" $input_size
+        elif [[ mod -eq 3 ]]; then
             # random + has duplicates
-            seq 1000 | sort -nr >source.tmp
-        elif [[ $mod==4 ]]; then
+            printf "%d\trandom \t yes\t" $input_size
+        elif [[ mod -eq 4 ]]; then
             # sorted + has duplicates
-            seq 1000 | sort -nr >source.tmp
-        elif [[ $mod==5 ]]; then
+            printf "%d\tsorted \t yes\t" $input_size
+        elif [[ mod -eq 5 ]]; then
             # reverse + has duplicates
-            seq 1000 | sort -nr >source.tmp
+            printf "%d\treverse\t yes\t" $input_size
         fi
 
-        # runing the sort
-        (time sort -n) < source.tmp 2>sys.tmp > /dev/null
-        grep real sys.tmp | cut -f2 >> sys_all.tmp
-        (time ./usel) < source.tmp 2>user.tmp > /dev/null
-        grep real user.tmp | cut -f2 >> user_all.tmp
-        i=`expr $i + 1`
+        printf "%d\t%lf\t%lf\t\n" $test_count $user $sys
+        # delete the file
+        rm -rf *.tmp
+
     done
-
-
-
-    # # write the data into files
-    # if [[ mod==0 ]]; then
-    #     #random + no duplicates
-    # elif [[ mod==1 ]]; then
-    #     # sorted + no duplicates
-    # elif [[ mod== 2 ]]; then
-    #     # reverse + no duplicates
-    # elif [[ mod==3 ]]; then
-    #     # random + has duplicates
-    # elif [[ mod==4 ]]; then
-    #     # sorted + has duplicates
-    # elif [[ mod==5 ]]; then
-    #     # reverse + has duplicates
-    # fi
-
-    # delete the file
-    # rm -rf *.tmp
 
 done
