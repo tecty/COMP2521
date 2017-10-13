e9e656f0640398fea8cdfadf2515b77b39b74eb5
Toby Hunag <619058900@qq.com>
2017-10-13 16:03:22 +1100

    finish the result calculation

--- week11/cal.c
+++ week11/cal.c
@@ -3,20 +3,28 @@
 
 int main(int argc, char const *argv[]) {
     int dump;
-    int nb,nbs,nk,nu;
+    long long int nk,nu;
+    double nb,nbs;
     double avg=0;
+    printf("%lf\n",avg );
     nk = 5000;
-    nk = 1666;
+    nu = 1666;
+    int depth = 0;
+    int depthAvg = 0;
     for (int i = 0; i < 10; i++) {
         // initial the values use in table
-        nb=nbs=nk=nu =0;
-        scanf("#nodes = %d,  depth = %d,  #compares = %d,  #rotates = %d",&dmup,&dmup,&nb,&dmup);
-        scanf("#nodes = %d,  depth = %d,  #compares = %d,  #rotates = %d",&dmup,&dmup,&nbs,&dmup);
+        nb=nbs =0;
+        scanf("#nodes = %d,  depth = %d,  #compares = %lf,  #rotates = %d",&dump,&depth,&nb,&dump);
+        depthAvg+= depth;
+        scanf("#nodes = %d,  depth = %d,  #compares = %lf,  #rotates = %d",&dump,&depth,&nbs,&dump);
         // calculate the correspond average
-        avg+= (nbs-nb)/(double)(nk+nu);
-
+        depthAvg += depth;
+        // printf("%d,%d",nk, nu);
+        // printf("%lld %lld\n", nbs,nb);
+        avg+= (nbs-nb)/((double)nk+nu);
+        printf("%lf\n",avg );
     }
     // print the average for all the test
-    print("%d",(int)(avg/10));
+    printf("%d %lf\n",depthAvg/20,(avg/10));
     return 0;
 }
