b0605698040848a685fa4579c13fc58f6bfa720b
Toby Huang <619058900@qq.com>
2017-10-16 04:12:21 +1100

    finish q2

--- sample_exam2/q2/hasPath.c
+++ sample_exam2/q2/hasPath.c
@@ -1,5 +1,5 @@
 
-/* hasPath.c 
+/* hasPath.c
    Written by Ashesh Mahidadia, October 2017
 */
 
@@ -10,29 +10,147 @@
 
 
 
-/* 
+/*
     You will submit only this one file.
 
-    Implement the function "hasPath" below. Read the exam paper for 
-    detailed specification and description of your task.  
+    Implement the function "hasPath" below. Read the exam paper for
+    detailed specification and description of your task.
 
-    - You can add helper functions in this file, if required.  
+    - You can add helper functions in this file, if required.
 
-    - DO NOT modify code in the file BSTree.h . 
-    - DO NOT add "main" function in this file. 
+    - DO NOT modify code in the file BSTree.h .
+    - DO NOT add "main" function in this file.
 */
 
 
 int *visited;  // array of visited
 
 
-int hasPath(Graph g, Vertex src, Vertex dest)
-{
+typedef struct node_t {
+    int val;
+    struct node_t *next;
+} *node;
 
-	// Implement this function, 
-	// also change the return value below!
+typedef struct queue_t {
+    node head;
+    node tail;
+} *queue;
 
-   return 0;
+node init_node(int val){
+    node new = malloc(sizeof(struct node_t));
+    assert(new != NULL);
+
+    // initial the value in the node
+    new->val = val;
+    new->next = NULL;
+}
+
+queue init_queue(){
+    queue new = malloc(sizeof(struct queue_t));
+
+    // initial the value in the queue
+    new ->head = new->tail = NULL;
+    return new;
 }
 
 
+int leave_q(queue q){
+    // pop the node in the queue
+    if (q->head == NULL) {
+        /* the queue is empty */
+        return -1;
+    }
+    // else
+    int this = q->head->val;
+    queue delete = q->head;
+    // rearrange the queue's link
+    q->head = q->head-> next;
+    if (q->head == NULL) {
+        /* the last node is leaved the queue, the queue is empty now */
+        q->head = NULL;
+    }
+
+    // free the memory usage
+    free(delete);
+    // return the Vertex in the node
+    return this;
+}
+
+void join_q(queue q,int val){
+    // create a node to link
+    node this = init_node(val);
+    if (q->head == NULL) {
+        /* the queue is empty */
+        q->head = q->tail = this;
+    }
+    else{
+        // add one more at the end of the queues
+        q->tail->next = this;
+        // refresh the queue
+        q->tail = this;
+    }
+}
+
+
+
+int hasPath(Graph g, Vertex src, Vertex dest)
+{
+
+	// Implement this function,
+	// also change the return value below!
+
+    // special situation
+    if (dest == src) {
+        /* the node can reach itself ? */
+        return 1;
+    }
+    if (g == NULL) {
+        /* not provided Graph */
+        return 0;
+    }
+
+
+    // initial the array of visited
+    visited = malloc(g->nV* sizeof(int));
+    for (int i = 0; i < g->nV; i++) {
+        /* initial the array with 0 */
+        visited[i] = 0;
+    }
+
+    // search for the Graph by bfs
+    // initial the queue to store the vertices
+    queue q = init_queue();
+
+    // join the first node to search
+    join_q(q , src);
+
+    // current search v
+    int v;
+
+    while ((v = leave_q(q))>=0) {
+        /* search for all the vertices could reach by src */
+        for (int to = 0; to < g->nV; to++) {
+            /* All possible edges from v to to */
+            if (adjacent(g, v, to)) {
+                /* here is an edge between v and to */
+                if (to == dest) {
+                    /* the node(to) is the required node */
+                    // successful found
+                    return 1;
+                }
+
+                if(visited[to]){
+                    // this node is visited, not going to visit it again
+                    // skip all the action to this node(to)
+                    continue;
+                }
+                // else
+                // set this node to visited, because it would be search
+                visited[to] = 1;
+                // join the queue, to search after
+                join_q(q, to);
+            }
+        }
+    }
+   return 0;
+}
